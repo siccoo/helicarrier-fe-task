2a824132245b56b5ca76de062dcf0229bd37aa31
Michael Chilaka <obichilaka@gmail.com>
2022-07-13 16:02:39 +0100

    updated-App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import './App.css';
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div className="App">
       Helicarrier Task
+      <SearchBar/>
     </div>
   );
 }
