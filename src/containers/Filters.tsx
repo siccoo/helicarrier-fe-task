4f3c3c93510e9440e38f43619888dd30f5090248
Michael Chilaka <obichilaka@gmail.com>
2022-07-14 11:24:10 +0100

    updates on filters.css

--- src/containers/Filters.tsx
+++ src/containers/Filters.tsx
@@ -3,24 +3,28 @@ import Button from '@mui/material/Button';
 import MultiSelectChip from '../components/MultiSelect';
 import { RangeSlider } from '../components/RangeSlider';
 import { DropdownWrapper } from '../components/DropdownWrapper';
+import "../styles/filters.css"
 
 export const Filters = () => {
-return(
-    <Box
-    sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        width: '600px',
-        backgroundColor: 'white',
-        borderColor: 'rgba(0, 0, 0, 1)',
-        borderWidth: "1px"
-    }}
-    >
-        <MultiSelectChip />
-        <DropdownWrapper title='Dropdown Menu' />
-        <RangeSlider />
-        <Button disabled>Clear All</Button>
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                width: '600px',
+                backgroundColor: 'white',
+                borderColor: 'rgba(0, 0, 0, 1)',
+                borderWidth: "1px"
+            }}
+        >
+            <div className='filtersContainer'>
+                <MultiSelectChip />
+                <DropdownWrapper title='Dropdown Menu' />
+            </div>
 
-    </Box>
-)
+            <RangeSlider />
+            <Button disabled>Clear All</Button>
+
+        </Box>
+    )
 }
